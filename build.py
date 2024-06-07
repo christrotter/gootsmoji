5231f9e4529c36b4a6d16bf23fbfbd4366ed8740
Alexander Keller <>
2024-06-07 17:46:41 -0400

    Refactor build script
    Moved the logic into a function so the module can be imported and used
    elsewhere.
    
    Factored out the configurable constants to the top of the file. I also
    reworded the descriptions for these to use a more extractable style. We
    can probably remove a bunch of the comments in the code since they
    explain what/how and not why, but for now they're fine to leave in.
    
    It now calculates the best number of columns to make the resulting grid
    a square.
    
    Added a final pass if the user has optipng installed to reduce the
    amount of space the image takes up.
    
    Ran black and pylint over it to fix up any issues and use a consistent
    style.
    
    Added a shebang and gave it execute privileges so it can just be run
    direct.
    
    Added missing newline to the end of file to make it POSIX compliant.
    
    Fixed a file descriptor leak by using a with context block when opening
    the image files.
    
    Sort the geets by name to stabilize the order.

--- build.py
+++ build.py
@@ -1,32 +1,63 @@
+#!/usr/bin/env python
+"""
+Build the README.md Goots grid.
+
+usage: ./build.py
+"""
+
+import math
 import os
+import shutil
+import subprocess
+
 from PIL import Image
 
-# copilot generated basically all of this code
 
-image_source_path = './goots'
+# IMAGE_SOURCE_DIR is the directory to list Goots images from.
+IMAGE_SOURCE_DIR = "./goots/"
 
-# Get a list of all image files in the image
-image_files = [f for f in os.listdir(image_source_path) if f.endswith(('.png'))]
+# OUTPUT_FILENAME is the filename to save the grid image to.
+OUTPUT_FILENAME = "grid_image.png"
 
-image_width = 100  # Width of each image in the grid
-image_height = 100  # Height of each image in the grid
+# THUMBNAIL_WIDTH and THUMBNAIL_HEIGHT are the dimensions to resize the geets in the grid.
+THUMBNAIL_WIDTH = 128
+THUMBNAIL_HEIGHT = 128
 
-# Determine the number of columns and rows for the grid
-num_columns = 6  # Number of columns in the grid
-num_rows = (len(image_files) + num_columns - 1) // num_columns  # Number of rows in the grid
-# Create a new image with the size of the grid
-grid_width = num_columns * image_width  # Assuming all images have the same width
-grid_height = num_rows * image_height  # Assuming all images have the same height
-grid_image = Image.new('RGB', (grid_width, grid_height))
-# Paste each image onto the grid
-for i, image_file in enumerate(image_files):
-    image_path = os.path.join(image_source_path, image_file)
-    image = Image.open(image_path)
-    image.thumbnail((image_width, image_height))  # Resize the image to fit the grid cell
-    row = i // num_columns
-    col = i % num_columns
-    x = col * image_width
-    y = row * image_height
-    grid_image.paste(image, (x, y))
-# Save the grid image
-grid_image.save('grid_image.png')
+
+def main():
+    """Generate the grid."""
+
+    # Get a list of all image files in the image.
+    image_files = [f for f in os.listdir(IMAGE_SOURCE_DIR) if f.endswith(".png")]
+    image_files.sort()
+
+    # Determine the number of columns and rows for the grid.
+    num_columns = math.ceil(math.sqrt(len(image_files)))
+    num_rows = (len(image_files) + num_columns - 1) // num_columns
+
+    # Create a new image with the size of the grid.
+    grid_width = num_columns * THUMBNAIL_WIDTH
+    grid_height = num_rows * THUMBNAIL_HEIGHT
+    grid_image = Image.new("RGB", (grid_width, grid_height))
+
+    # Paste each image onto the grid.
+    for i, image_file in enumerate(image_files):
+        image_path = os.path.join(IMAGE_SOURCE_DIR, image_file)
+        with Image.open(image_path) as image:
+            image.thumbnail((THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT))
+            row = i // num_columns
+            col = i % num_columns
+            x = col * THUMBNAIL_WIDTH
+            y = row * THUMBNAIL_HEIGHT
+            grid_image.paste(image, (x, y))
+
+    # Save the grid image.
+    grid_image.save(OUTPUT_FILENAME)
+
+    # Compress using optiping if installed.
+    if shutil.which("optipng"):
+        subprocess.run(["optipng", "-o9", OUTPUT_FILENAME], check=True)
+
+
+if __name__ == "__main__":
+    main()
